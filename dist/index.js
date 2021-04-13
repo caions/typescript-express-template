6d31178cb7c99c8782ec2a79385abb26ad032f06
Caio Nascimento <caionsantos2000@gmail.com>
2021-04-13 16:39:04 -0300

    multer config ok

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
 const app = express_1.default();
 const PORT = 8000;
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.use(routes_1.default);
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
---
+++ dist/routes.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const multer_1 = require("./config/multer");
+const multer_2 = __importDefault(require("multer"));
+const routes = express_1.Router();
+routes.post('/teste', multer_2.default(multer_1.multerConfig).single('file'), (request, response) => {
+    console.log(request.file);
+    return response.json(request.file);
+});
+exports.default = routes;
