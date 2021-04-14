a951b1d5057f716d68b685f92a11813f1debfd81
Caio Nascimento <caionsantos2000@gmail.com>
2021-04-13 21:59:06 -0300

    finished config

--- routes.ts
+++ routes.ts
@@ -1,8 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { multerConfig } from './config/multer';
+import multer from 'multer';
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json('deu bom');
-});
+routes.post(
+  '/upload',
+  multer(multerConfig).single('file'),
+  (request: Request, response: Response) => {
+    return response.json(request.file);
+  },
+);
 
 export default routes;
